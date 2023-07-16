64458f5e0040a7890e0c6e08a78b11a2a144870d
dnovelty <45954889+dnovelty@users.noreply.github.com>
2023-07-16 10:54:12 +0800

    Update cm520-openwrt-stable-custom-script-1.sh
    third packages

--- cm520-openwrt-stable-custom-script-1.sh
+++ cm520-openwrt-stable-custom-script-1.sh
@@ -150,5 +150,5 @@ git config --global user.name "Curious"
 git am opboot.patch
 
 sed -i 's/src-git-full/src-git/g' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 git clone --depth 1 --branch js  --single-branch https://github.com/gngpp/luci-theme-design.git package/luci-theme-design/
-git clone --depth 1 https://github.com/kiddin9/openwrt-packages.git package/third-packages/
