9bffcfd180e86e464f9056b3fe8d91d3247c4a93
dnovelty <45954889+dnovelty@users.noreply.github.com>
2026-01-01 09:06:08 +0800

    Comment out feed sources and ddns-go clone
    Comment out feed source additions and cloning of ddns-go.

--- cm520-lean-lede-custom-script-1.sh
+++ cm520-lean-lede-custom-script-1.sh
@@ -36,6 +36,6 @@
 #EOF
 #git apply example.patch
 
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small.git # passwall 基本依赖' >> feeds.conf.default
-git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go/
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small.git # passwall 基本依赖' >> feeds.conf.default
+#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go/
